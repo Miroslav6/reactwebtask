80d8a27d6dc7a7d44697febb241d62871b5ed3c5
Miroslav6 <miroslav.nedev1@gmail.com>
2022-02-08 14:34:26 +0200

    Added option for select album
    Added option for select album

--- src/pages/albums.jsx
+++ src/pages/albums.jsx
@@ -1,16 +1,31 @@
 import { React, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { addFavourite } from '../actions';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import api from '../api/posts'
 
 const Albums = () => {
   const dispatch = useDispatch();
+  const stateFavorites = useSelector(state => state.addFavourite);
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const stateFavorites = useSelector(state => state.addFavourite);
-  const [favorites, setFavorites] = useState(stateFavorites); 
+  const [favorites, setFavorites] = useState(stateFavorites);
+  const [albumid, setAlbumId] = useState('Select Album');
+  const albumIds = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    'All Albums'
+  ];
+
   useEffect(() => {
-     if(favorites) {
+    if (favorites) {
       setFavorites(favorites);
     }
 
@@ -24,7 +39,25 @@ const Albums = () => {
         const album4 = response.filter(album => album.albumId === 4);
         const album5 = response.filter(album => album.albumId === 5);
         const albums = ([...album1, ...album2, ...album3, ...album4, ...album5]);
-        setData(albums);
+        
+        if(albumid === 1) {
+          setData(album1);
+        }
+        else if(albumid === 2) {
+          setData(album2);
+        }
+        else if(albumid === 3) {
+          setData(album3);
+        }
+        else if(albumid === 4) {
+          setData(album4);
+        }
+        else if(albumid === 5) {
+          setData(album5);
+        }
+        else{
+          setData(albums);
+        }
       } catch (error) {
         console.error(error.message);
       }
@@ -33,10 +66,14 @@ const Albums = () => {
 
     fetchData();
     dispatch(addFavourite(favorites));
-  }, [favorites]);
+  }, [albumid, favorites]);
 
   const onBtnFavourites = (album) => {
     setFavorites([...favorites, album]);
+  };
+
+  const handleSelectAlbumId = (e) => {
+    setAlbumId(e.target.value);
   };
 
   return (
@@ -45,17 +82,39 @@ const Albums = () => {
         {loading && <div>Loading</div>}
         {!loading && (
           <div>
+            <div className='col col-sm-3 mb-5 stock-item'>
+              <Box>
+                <FormControl fullWidth>
+                  <InputLabel id="select-label">Select Album</InputLabel>
+                  <Select
+                    labelId="select-label"
+                    id="height-select"
+                    value={albumid}
+                    label="Select Album"
+                    onChange={handleSelectAlbumId}
+                  >
+                    {albumIds?.map(data => {
+                      return (
+                        <MenuItem key={data} value={data}>
+                          {data}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
             <div className='row stock-container'>
               {data.map((album, key) => {
                 return (
                   <div className='col col-sm-12 col-md-6 col-lg-3  mb-5 text-center' key={key}>
-                    <div className='products-item h-100 '>
-                      <a href={'/albums/' + album.albumId + '-' + album.id} className='d-block p-3 pt-5'>
+                    <div className='products-item p-5 h-100 '>
+                      <a href={'/albums/' + album.albumId + '-' + album.id} className='d-block'>
                         <div>{album.url ? <img loading="lazy" src={album.thumbnailUrl} alt="Album" /> : ''}</div>
                         <h3 className='pb-2 border-bottom'>AlbumID - {album.albumId}</h3>
                         <h3 className='pb-2 '>{album.title}</h3>
                       </a>
-                      <span onClick={() => onBtnFavourites(album)}><i className="bi bi-heart fa-sm"></i>Add to Favourites</span>
+                      <button onClick={() => onBtnFavourites(album)} className='btn btn-primary'>Add card</button>
                     </div>
                   </div>
                 );
--- src/pages/favourites.jsx
+++ src/pages/favourites.jsx
@@ -6,29 +6,35 @@ const Favourites = () => {
   const dispatch = useDispatch();
   const stateFavorites = useSelector(state => state.addFavourite);
 
-    const onDeleteFavourites = (album) => {
-      dispatch(removeFavourite(album));
-    };
-    
+  const onDeleteFavourites = (album) => {
+    dispatch(removeFavourite(album));
+  };
+
   return (
     <>
       <div className='container mt-5'>
-     
         <div className='row stock-container'>
-               { stateFavorites.length ? stateFavorites.map((album, key) => {
-                  return (
-                    <div className='col col-sm-12 col-md-6 col-lg-3 mb-5 text-center' key={key}>
-                      <a onClick={() => onDeleteFavourites(album)} className='h-100 delete-card'>
-                      <div className='products-item p-5 h-100 '>
-                          <div>{album.url ? <img loading="lazy" src={album.thumbnailUrl} alt="Album" /> : ''}</div>
-                          <h3 className='pb-2 border-bottom'>AlbumID - {album.albumId}-{album.id}</h3>
-                          <h3 className='pb-2 '>{album.title}</h3>
-                      </div>
-                      </a>
-                    </div>
-                  );
-                }) : <p className='col col-sm-12 display-4 text-center'><i className="bi bi-heart fa-sm"></i> No Favourites</p>}
-                
+        {stateFavorites.length ? 
+          <div className='col-12'>
+            <h1>Favourites</h1>
+          </div>
+          : ''
+          }
+          {stateFavorites.length ? stateFavorites.map((album, key) => {
+            return (
+              <div className='col col-sm-12 col-md-6 col-lg-3 mb-5 text-center' key={key}>
+
+                <div className='products-item p-5 h-100 '>
+                  <div>{album.url ? <img loading="lazy" src={album.thumbnailUrl} alt="Album" /> : ''}</div>
+                  <h3 className='pb-2 border-bottom'>AlbumID - {album.albumId}-{album.id}</h3>
+                  <h3 className='pb-2 '>{album.title}</h3>
+                  <button onClick={() => onDeleteFavourites(album)} className='btn btn-primary'>Remove card</button>
+                </div>
+
+              </div>
+            );
+          }) : <p className='col col-sm-12 display-4 text-center'><i className="bi bi-heart fa-sm"></i> No Favourites</p>}
+
         </div>
       </div>
     </>
